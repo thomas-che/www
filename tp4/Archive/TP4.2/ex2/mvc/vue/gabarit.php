d615c84f09fd171bb88ded778edf981231d30a22
thomas-ubuntu <thomas-pc-dell@saugarde>
2019-11-29 01:03:34 +0100

    tp4 ex2 partie 3 fini: MVC

--- tp4/Archive/TP4.2/ex2/mvc/vue/gabarit.php
+++ tp4/Archive/TP4.2/ex2/mvc/vue/gabarit.php
@@ -8,8 +8,37 @@
     </head>
     
 	<body>	
-	  
-  </body>
+	    <form id="addCustomer" action="site.php" method="post">
+   			<fieldset>
+   				<legend>Ajouter client</legend>
+				<p><label> Nom : </label><input type="text" name="lastName" /></p>
+				<p><label> Prenom : </label><input type="text" name="name" /></p>
+				<p><label> Date naissance : </label><input type="date" name="birthDate" /></p>
+				<p><label> Tel : </label><input type="text" name="phoneNumber" /></p>
+				<p> <input type="submit" value="Ajouter client" name="addCustomer" /> </p>
+				<p> <input type="reset" value="Tout effacer" name="clear" /> </p>
+			</fieldset>
+		</form>
+
+		<form id="displayCustomer" action="site.php" method="post">
+   			<fieldset>
+   				<legend>Afficher client</legend>
+				<p> <input type="submit" value="Afficher client" name="displayCustomer" /> </p>
+			</fieldset>
+		</form>
+
+		<?php echo $displayContents ; ?>
+
+		<form id="getCustomer" action="site.php" method="post">
+   			<fieldset>
+   				<legend>Rechercher client</legend>
+				<p><label> Nom client : </label><input type="text" name="getCustomerName" /></p>
+				<p> <input type="submit" value="Rechercher client" name="getCustomer" /> </p>
+				<p> <input type="reset" value="Tout effacer" name="clear" /> </p>
+			</fieldset>
+		</form>
+
+  	</body>
 
  </html>
  
