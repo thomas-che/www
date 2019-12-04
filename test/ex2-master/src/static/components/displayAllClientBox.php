acdd87aed02552b2a943daad52208fbe06be5d0c
thomas-ubuntu <thomas-pc-dell@saugarde>
2019-12-04 19:38:26 +0100

    mini_chat en mvc

--- test/ex2-master/src/static/components/displayAllClientBox.php
+++
@@ -1,9 +0,0 @@
-<div class="row">
-    <form class="col s6 offset-s3 z-depth-3" method="POST">
-        <div class="row center">
-            <button class="btn waves-effect waves-light" type="submit" name="displayAllClient" style="margin-top: 2%">display all user
-                <i class="material-icons right">send</i>
-            </button>
-        </div>
-    </form>
-</div>
--- tp/tp4/td4/td4/ex2/non_mvc/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("SERVEUR","localhost");
-define("USER","tp");
-define("PASSWORD","tp");
-define("BDD","td4_ex2");
-?>
